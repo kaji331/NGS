79756ad5cef5eabb9067b0254e31c9c6a65d27b6
kaji331 <kaji331@hotmail.com>
2015-06-13 01:30:54 +0800

    initial commit 批量trim处理fastq.gz的脚本示例

---
+++ Trim_script/trim.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+IFS=$(echo -en "\n\b")
+for i in `ls .. | grep .fastq.gz$`
+do
+	name=$(echo $i | sed 's/\.fastq.gz/\_trimmed.fastq.gz/')
+	mkdir -p "../Trimmed"
+	java -jar "trimmomatic-0.33.jar" "SE" -phred33 -threads "2" -trimlog "../$name.log" "../$i" "../Trimmed/$name" "ILLUMINACLIP:TruSeq3-SE.fa:2:30:10" "ILLUMINACLIP:SmartSeq.fa:2:30:10" "HEADCROP:14" "LEADING:30" "TRAILING:30" "SLIDINGWINDOW:4:30" "MINLEN:50"
+done
