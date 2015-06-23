be8b3fe2316cc4657d179cecb459484bb10981b8
kaji331 <kaji331@hotmail.com>
2015-06-23 21:51:45 +0800

    使用NCBI的GRCh38参考基因组

--- Tophat_script/tophat_all.sh
+++ Tophat_script/tophat_all.sh
@@ -4,6 +4,5 @@ IFS=$(echo -en "\n\b")
 for i in `find .. -iname "*_trimmed.fastq.gz" -print`
 do
 	name=$(echo $i | sed 's/\_trimmed.fastq.gz/\_mapping/')
-	mkdir -p "$name"
-	tophat -p "8" -o "$name" --bowtie1 --fusion-search "/share/home/xierf/Yang_L/Ref_hg38/hg38_index/hg38" "$i"
+	tophat -p "8" -o "$name" -G "/share/home/xierf/Yang_L/Ref_hg38/NCBI_GRCh38/GCA_000001405.15_GRCh38_full_analysis_set.refseq_annotation.gff" "/share/home/xierf/Yang_L/Ref_hg38/NCBI_GRCh38/GCA_000001405.15_GRCh38_no_alt_analysis_set.fna.bowtie_index" "$i"
 done
